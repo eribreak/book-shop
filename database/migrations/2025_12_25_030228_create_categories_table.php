7df5d735f5b348d015994e6f3ca49276817789e8
eribreak <transyhieu987@gmail.com>
2025-12-25 11:03:09 +0700

    feat: initial project

---
+++ database/migrations/2025_12_25_030228_create_categories_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->varchar(255);
+            $table->text('description');
+            $table->string('slug')->unique();
+            $table->tinyInteger('is_home')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('categories');
+    }
+};
